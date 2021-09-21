0182ad264dfa311aa68b03a67997d4f350e39f31
alejandrolederman <alejandro.lederman@live.com.ar>
2021-09-21 14:20:01 -0300

    ejercicio concesionaria

---
+++ appconcesionaria.js
@@ -0,0 +1,94 @@
+let autos = require ("./listaAutos")
+let personas = require ("./personas")
+
+let concesionaria = {
+   autos: autos,
+ 
+  buscarAuto: function (unaPatente){
+      for ( let i = 0; i < this.autos.length; i++){
+if (unaPatente == this.autos[i].patente){
+return this.autos[i];
+}
+}
+    return null; 
+},
+
+   venderAuto: function  (unaPatente){
+      let unAuto = this.buscarAuto (unaPatente);
+         return unAuto.vendido = true;
+       },
+
+   autosParaLaVenta: function (){
+      let paraVender = autos.filter (function (auto){
+         return auto.vendido == false;
+      });
+      return paraVender;
+   },
+   
+autosNuevos: function(){
+        let enVenta = this.autosParaLaVenta();
+        let ceroKm = enVenta.filter(function(enVenta){
+            return enVenta.km < 100;
+        });
+        return ceroKm;
+    },
+
+
+    listaDeVentas: function (){
+        let autosVendidos = this.autos.filter(function(auto){
+           return auto.vendido == true;
+        })
+
+        let precioAutos = autosVendidos.map (function (unAuto){
+           return unAuto.precio;
+        })
+        
+        return precioAutos;
+  },
+
+  totalDeVentas: function(){
+      let listaVenta = this.listaDeVentas();
+
+        if (listaVenta == 0){
+               
+         return 0;
+
+        }  else{
+
+            let totalVentas = listaVenta.reduce (function (acum, precio){
+                return acum + precio; } )
+                return totalVentas;
+        }    
+    },
+
+    puedeComprar : function( unAuto, unaPersona){
+
+        listaAutos = this.autosParaLaVenta();
+              console.log (listaAutos);
+      if(listaAutos == unAuto){
+          let valorCuota = unAuto.precio / unAuto.cuotas;
+          
+      }
+
+           
+  
+  
+  
+  
+  
+     }
+
+
+
+
+  }
+
+
+
+
+
+
+
+  console.log (concesionaria.puedeComprar(autos,personas));
+
+
---
+++ listaAutos.js
@@ -0,0 +1,53 @@
+let  autos =[
+    {
+   marca: "Ford ",
+   modelo: "Fiesta",
+   precio: 150000,
+   km: 200 ,
+   color: " Azul",
+   cuotas: 12,
+   anio: 2019,
+   patente: "APL123",
+   vendido: false ,
+} ,
+
+{ 
+   marca: "Toyota",
+   modelo: "Corolla",
+   precio: 100000,
+   km: 0,
+   color: "Blanco",
+   cuotas: 14,
+   anio: 2019,
+   patente: "JJK116",
+   vendido: true ,
+} ,
+
+{ 
+   marca: "peugeot",
+   modelo: "306",
+   precio: 150000,
+   km: 150,
+   color: "Blanco",
+   cuotas: 12,
+   anio: 1998,
+   patente: "ABB523",
+   vendido: false ,
+},
+
+{ 
+   marca: "citroen",
+   modelo: "pacasso",
+   precio: 235000,
+   km: 35,
+   color: "gris",
+   cuotas: 20,
+   anio: 2021,
+   patente: "NRF314",
+   vendido: true ,
+}
+
+]
+
+
+module.exports = autos;
